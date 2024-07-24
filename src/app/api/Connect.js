68ce34838988006b0ed7df582ccba691afc73169
DhrumitPanchal <dhrumitpanchal789@gmail.com>
2024-07-24 23:40:32 +0530

    remove local DB url

--- src/app/api/Connect.js
+++ src/app/api/Connect.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 export const Connect = async () => {
+  const DB_URL = process.env.NEXT_PUBLIC_DB_URL;
   try {
-    mongoose.connect("mongodb://localhost:27017/easy-forex");
+    mongoose.connect(DB_URL);
     console.log("mongoDB connected");
   } catch (error) {
     console.log("DB connection error : " + error);
