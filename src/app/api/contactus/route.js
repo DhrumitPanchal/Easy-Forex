b9b613262f309c66dff1e553e240bff6d2d8b083
DhrumitPanchal <dhrumitpanchal789@gmail.com>
2024-08-01 20:27:36 +0530

    change sender in contactus api

--- src/app/api/contactus/route.js
+++ src/app/api/contactus/route.js
@@ -4,9 +4,10 @@ import { transporter } from "../payment/nodemailerConfig";
 
 export async function POST(req) {
   const { subject, message, name, email } = await req.json();
+
   try {
     const info = await transporter.sendMail({
-      from: "East-Forex Web",
+      from: '"Easy-Forex ContactUs" dhrumit6789@gmail.com',
       to: "dhrumitpanchal789@gmail.com",
       subject: subject,
       html: `<h4>Subject : <b>${subject}</b></h4> <p>${message}</p> <h4><b>Name : ${name}</b>,<br><b> Email : ${email}</b></h4>`,
